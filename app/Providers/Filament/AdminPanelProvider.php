83192a37ac4147527ba944b07700b305b5848f98
teguhpermadi <tguh.permadi@gmail.com>
2025-11-26 10:49:31 +0700

    feat: Configure Filament admin panel.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -128,6 +128,7 @@ class AdminPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->databaseNotifications()
-            ->databaseNotificationsPolling('10s');
+            ->databaseNotificationsPolling('10s')
+            ->topNavigation();
     }
 }
