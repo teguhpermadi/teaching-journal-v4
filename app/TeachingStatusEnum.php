a4330c4a396d814c56a26d506303ce98d3708971
teguhpermadi <tguh.permadi@gmail.com>
2025-11-28 13:58:57 +0700

    faet : add sumatif in teaching status enum

--- app/TeachingStatusEnum.php
+++ app/TeachingStatusEnum.php
@@ -7,14 +7,16 @@ use Filament\Support\Contracts\HasLabel;
 enum TeachingStatusEnum: string implements HasLabel
 {
     case PEMBELAJARAN = 'Pembelajaran';
-    case PENILAIAN = 'Penilaian';
+    case PENILAIAN = 'Formatif';
+    case SUMATIF = 'Sumatif';
     case DITIADAKAN = 'Ditiadakan';
 
     public function getLabel(): ?string
     {
         return match ($this) {
             self::PEMBELAJARAN => 'Pembelajaran',
-            self::PENILAIAN => 'Penilaian',
+            self::PENILAIAN => 'Formatif',
+            self::SUMATIF => 'Sumatif',
             self::DITIADAKAN => 'Pembelajaran Ditiadakan',
         };
     }
@@ -24,6 +26,7 @@ enum TeachingStatusEnum: string implements HasLabel
         return match ($this) {
             self::PEMBELAJARAN => 'success',
             self::PENILAIAN => 'warning',
+            self::SUMATIF => 'secondary',
             self::DITIADAKAN => 'danger',
         };
     }
